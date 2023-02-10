a0c66d7bbdd65173ceb37771337f5d6e7a1b2e5b
Wlad Kowalew <wladickowalew@gmail.com>
2023-02-10 18:39:19 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Привет, Яндекс!"
+
+
+@app.route('/countdown')
+def countdown():
+    countdown_list = [str(x) for x in range(10, 0, -1)]
+    countdown_list.append('Пуск!')
+    return '</br>'.join(countdown_list)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
