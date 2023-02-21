04e8944abc676b9b34035a55d16210facb762e7e
Wlad Kowalew <wladickowalew@gmail.com>
2023-02-21 18:49:01 +0300

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+from werkzeug.utils import redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет, Яндекс!"
+    user = "Ученик Яндекс.Лицея"
+    return render_template('index.html', title='Домашняя страница',
+                           username=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def odd_even():
+    return "УРАААААААА!!!!!!"
+
+
+@app.route('/odd_even')
+def odd_even2():
+    return render_template('odd_even.html', number=2)
 
 
 @app.route('/countdown')
