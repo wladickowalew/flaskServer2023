82f0e3c9d0aff400140fd5add98e7e53de5edbf2
Wlad Kowalew <wladickowalew@gmail.com>
2023-02-28 19:02:45 +0300

    new commit

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template
 from werkzeug.utils import redirect
 
+from data.news import News
 from data.users import User
-from loginform import LoginForm
+from forms.loginform import LoginForm
 from data import db_session
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 db_session.global_init("db/blogs.db")
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
 def index():
-    user = "Ученик Яндекс.Лицея"
-    return render_template('index.html', title='Домашняя страница',
-                           username=user)
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -26,20 +27,35 @@ def login():
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @app.route('/success')
-def odd_even():
+def success():
     return "УРАААААААА!!!!!!"
 
-def test():
-    db_sess = db_session.create_session()
-    for i in range(2, 101):
-        user = User()
-        user.name = f"Пользователь {i}"
-        user.about = f"биография пользователя {i}"
-        user.email = f"email{i}@email.ru"
-        db_sess.add(user)
-    db_sess.commit()
 
 if __name__ == '__main__':
-    test()
     app.run(port=8080, host='127.0.0.1')
